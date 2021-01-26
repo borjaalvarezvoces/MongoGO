9bb9f44ae121eaf9e19c771e95151effeae26660
borjaalvarezvoces <alvarezvocesborja@gmail.com>
2021-01-26 17:36:42 +0100

    Initial commit

---
+++ app/src/main/java/com/borja/mongogo/DetailsActivity.kt
@@ -0,0 +1,131 @@
+package com.borja.mongogo
+
+import android.app.Activity
+import android.app.PendingIntent.getActivity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.widget.HorizontalScrollView
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_details.*
+import java.io.Serializable
+import java.text.DateFormat
+import java.util.*
+
+
+
+class DetailsActivity : AppCompatActivity(), Serializable {
+
+
+    private lateinit var markerTxtId: TextView
+    private lateinit var dateToTxt: TextView
+
+    val permissions = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    private val PERMISSIONS_REQUEST_ACCESS_CAMERA_AND_WSTORAGE = 1001
+    private val IMAGE_CAPTURE_CODE = 1002
+
+    private val arrayImageViews: MutableList<Uri> = mutableListOf()
+    var image_uri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        dateForMarker()
+
+        markerTxtId = findViewById(R.id.descriptionDetailTxt)
+        val markerIdDetail = intent.getSerializableExtra("Id")
+        markerTxtId.text = markerIdDetail.toString()
+
+        var inflater = LayoutInflater.from(this)
+
+        button_capture.setOnClickListener{
+            takePhoto()
+        }
+
+    }
+    private fun takePhoto(){
+        if(hasNoPermissions()){
+            requestPermission()
+        } else {
+            openCamera()
+        }
+    }
+    private fun hasNoPermissions():Boolean{
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        } else {
+            return true
+        }
+    }
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(this, permissions, PERMISSIONS_REQUEST_ACCESS_CAMERA_AND_WSTORAGE)
+    }
+
+    private fun dateForMarker(){
+        val calendar = Calendar.getInstance()
+        val currentDate = DateFormat.getDateInstance().format(calendar.getTime())
+        dateToTxt = findViewById(R.id.dateDetailTxt)
+        dateToTxt.text = currentDate
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture B")
+        values.put(MediaStore.Images.Media.DESCRIPTION,"From the camera B")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+        //Camera intent
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == PERMISSIONS_REQUEST_ACCESS_CAMERA_AND_WSTORAGE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openCamera();
+            } else {
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show()
+            }
+        } else{
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //called when image was captured from camera intent
+        if (resultCode == Activity.RESULT_OK){
+            image_uri?.let { arrayImageViews.add(it) }
+            displayArrayOfViews()
+        }
+    }
+
+   private fun  displayArrayOfViews(){
+        for (i in 1.. arrayImageViews.size){
+            val newView = ImageView(this)
+            newView.setImageURI(image_uri)
+            imageLayout.addView(newView)
+        }
+   }
+}
+
---
+++ app/src/main/java/com/borja/mongogo/MapsActivity.kt
@@ -0,0 +1,182 @@
+package com.borja.mongogo
+
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.CameraPosition
+
+import java.util.*
+
+
+class MapsActivity : AppCompatActivity(),
+    OnMapReadyCallback,
+    GoogleMap.OnMarkerClickListener,
+    GoogleMap.OnInfoWindowLongClickListener {
+
+    private var map: GoogleMap? = null
+
+    private lateinit var pointMarker: Marker
+    private val arrayOfMarkers = arrayListOf<Marker>()
+
+    private var lastKnownLocation: Location? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1000
+    val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        // Build the map.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation()
+
+        setMapLongClick(map)
+        deleteMarkerButton()
+        // map.setOnInfoWindowClickListener(this)
+        map.setOnInfoWindowLongClickListener(this)
+        map.setOnMarkerClickListener(this)
+    }
+
+    private fun getDeviceLocation() {
+        if (hasNoPermissions()) {
+            requestPermission()
+        } else {
+            getUserLocation()
+        }
+    }
+
+    private fun hasNoPermissions(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        } else {
+            return true
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            permissions,
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+        )
+    }
+
+    private fun getUserLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+                lastKnownLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map?.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getUserLocation();
+            } else {
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.3f, Long: %2$.3f",
+                latLng.latitude,
+                latLng.longitude
+            )
+            pointMarker = map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.marker_title))
+                    .draggable(true)
+                    .snippet(snippet)
+            )
+            pointMarker.tag = 0
+            map.animateCamera(CameraUpdateFactory.newLatLng(pointMarker.position))
+            arrayOfMarkers.add(pointMarker)
+            println(pointMarker.id)
+            println(arrayOfMarkers)
+        }
+    }
+
+    private fun deleteMarkerButton() {
+        val btnDeleteMarker = findViewById<Button>(R.id.btn_delete_marker)
+        btnDeleteMarker.setOnClickListener {
+            println(arrayOfMarkers)
+            arrayOfMarkers.removeAt(1).remove()
+            println(arrayOfMarkers)
+        }
+    }
+
+    override fun onInfoWindowLongClick(marker: Marker?) {
+        println(marker?.id)
+        println(pointMarker.id)
+
+        val markerId = marker?.id
+        val intentMarker = Intent(this, DetailsActivity::class.java)
+        intentMarker.putExtra("Id", markerId)
+        startActivity(intentMarker)
+    }
+
+    override fun onMarkerClick(marker: Marker?): Boolean {
+        return false
+    }
+}
+
+
+
